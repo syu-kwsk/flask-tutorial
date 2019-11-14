df166446df27e0733b7d9455d6671702ce226064
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-11-14 16:57:02 +0900

    Add login and show user_date_data

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,10 +1,16 @@
 from flask import request, redirect, url_for, render_template, flash
 from flaskr import app, db
-
-@app.route('/')
+from flaskr.models import User, Date 
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    name = "syukwsk"
-    return render_template('index.html', name=name)
+    if request.method == 'POST':
+        user = request.form['name']
+        user = User.query.filter(User.name == user).first()
+        if user is None:
+            flash('Invalid User Name')
+        else:
+            return render_template('home.html', user=user)
+    return render_template('index.html')
 
 @app.route('/attend/<string:name>')
 def attend(name):
